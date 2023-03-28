211c354ae5f084fa041ceb079c5638f252449d6b
Markus Obermaier <markus.obermaier420@gmail.com>
2023-03-28 14:49:25 +0200

    layout

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
-# Sveltekit-Tailwind Boilerplate
+# pdf-analyzer
+
+Uses GPT-3.5 and Pinecone to process large pdf.Gather information about the pdf-file by chatting with a chatbot
+
+This repo is inspired by **[https://github.com/mayooear/gpt4-pdf-chatbot-langchain](https://github.com/sveltejs/kit)**
 
 ```bash
+# ingest pdf into pinecone by converting it into vectors
+just ingest
 # start development server
 just dev
 # build project
@@ -14,6 +20,7 @@ just lint
 ```
 
 ## Techstack
+
 - full-stack framework: **[SvelteKit](https://github.com/sveltejs/kit)**
 - css framework: **[Tailwind](https://github.com/tailwindlabs/tailwindcss)**
 - packet manager: **[pnpm](https://github.com/pnpm/pnpm)**
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,39 @@
 <script>
 	import '../app.css';
+
+	const navigation = [
+		{
+			name: 'Home',
+			href: '/#'
+		},
+		{
+			name: 'About',
+			href: '/#'
+		}
+	];
 </script>
 
-<slot />
+<div class="mx-auto flex flex-col space-y-4">
+	<header class="container sticky top-0 z-40 bg-white">
+		<div class="flex h-16 border-b border-b-slate-200 py-4 bg-gray-200">
+			<nav-left class="ml-4 pl-6">
+				<ul class="flex space-x-2">
+					{#each navigation as item}
+						<li>
+							<a href={item.href} class="hover:text-slate-600 cursor-pointer">
+								{item.name}
+							</a>
+						</li>
+					{/each}
+				</ul>
+			</nav-left>
+			<nav-right>
+				<div class="absolute right-10">PDF</div>
+			</nav-right>
+		</div>
+	</header>
+
+	<main class="flex w-full flex-1 flex-col overflow-hidden">
+		<slot />
+	</main>
+</div>
