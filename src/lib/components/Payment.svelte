9d2c2fde3f46192dba4bb0b1b144cab4681722fc
Markus Obermaier <markus.obermaier420@gmail.com>
2023-04-01 00:48:41 +0200

    modal

--- src/lib/components/Payment.svelte
+++ src/lib/components/Payment.svelte
@@ -10,10 +10,22 @@
 
 <input type="checkbox" id="my-modal-3" class="modal-toggle" />
 <div class="modal">
-  <div class="modal-box relative">
+  <div class="modal-box relative flex flex-col justify-center items-center space-y-8">
     <label for="my-modal-3" class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
-    <h3 class="text-lg font-bold">Congratulations random Internet user!</h3>
-    <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+    <label>
+		Cryptocurrency type:
+		<select bind:value={cryptoType}>
+			<option value="BTC">Bitcoin</option>
+			<option value="ETH">Ethereum</option>
+			<option value="LTC">Litecoin</option>
+		</select>
+	</label>
+	<label>
+		Amount:
+		<input type="number" step="0.0001" bind:value={amount} />
+    </label>
+	<input hidden type="text" bind:value={walletAddress} />
+	<button type="submit">Pay</button>
   </div>
 </div>
 
