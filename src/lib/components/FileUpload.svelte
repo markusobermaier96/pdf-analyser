a58905ca5a5bc61110a068379e8a3237fb9293ad
Markus Obermaier <markus.obermaier420@gmail.com>
2023-04-02 12:35:39 +0200

    file upload working

--- src/lib/components/FileUpload.svelte
+++ src/lib/components/FileUpload.svelte
@@ -3,7 +3,6 @@
 	import toast from 'svelte-french-toast';
 
 	const handleSubmit: SubmitFunction = async ({ form, data, action, cancel, submitter }) => {
-		console.log(data.get('pdf'));
 		if (!(data.has('pdf') && data.get('pdf')?.size != 0)) {
 			toast.error('Please choose a pdf file.');
 			cancel();
@@ -12,19 +11,27 @@
 			cancel();
 		}
 		return async ({ result, update }) => {
-			console.log(result);
+			if (result.type == 'success') {
+				toast.success('Successfully uploaded file');
+				document.getElementById('modalClose')?.click();
+			} else {
+				toast.error('Error occured. Try again');
+			}
+			update();
 		};
 	};
 </script>
 
 <input type="checkbox" id="file-upload" class="modal-toggle" />
 <div class="modal">
-	<div class="modal-box relative flex flex-col justify-center items-center">
+	<div class="modal-box relative">
 		<form method="POST" use:enhance={handleSubmit} action="?/upload">
 			<input name="pdf" type="file" class="file-input file-input-ghost w-full max-w-xs" />
 			<button class="border p-2 border-gray-300 bg-blue-400 rounded-lg" type="submit">Upload</button
 			>
 		</form>
-		<label for="file-upload" class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+		<label id="modalClose" for="file-upload" class="btn btn-sm btn-circle absolute right-2 top-2"
+			>✕</label
+		>
 	</div>
 </div>
--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,34 +1,19 @@
-import * as fs from 'fs';
+import type { Actions } from './$types';
+import { promises as fs } from 'fs';
 
 export const actions: Actions = {
   upload: async ({ cookies, request }) => {
     const data = await request.formData();
-    const file = data.get('pdf');
-    if (!file) {
-      throw new Error('no file available');
+    const fileField = data.get('pdf'); // value of 'name' attribute of input
+    if (!fileField || !(fileField instanceof File)) {
+      return "error";
     }
-  
-    const fileName = file.name;
-    const fileContent = file instanceof Blob ? file : file.buffer;
-    console.log("file name ", fileName)
-    console.log("file content ", fileContent)
-    const fileObject = new File([fileContent], fileName);
-
-    // Create the directory if it doesn't exist
-    await fs.promises.mkdir('docs', { recursive: true });
-    const filePath = `docs/${file.name}`;
-    const writeStream = fs.createWriteStream(filePath);
-
-    await new Promise<void>((resolve, reject) => {
-      readStream.pipe(writeStream);
-
-      readStream.on('error', reject);
-
-      writeStream.on('error', reject);
-
-      writeStream.on('finish', resolve);
-    });
-
-    return 'File uploaded successfully';
-  },
-};
+    const fileBuffer = await fileField.arrayBuffer();
+    const filePath = `./docs/${fileField.name}`;
+    await fs.writeFile(filePath, Buffer.from(fileBuffer));
+    return {
+      status: 200,
+      body: 'PDF file uploaded successfully',
+    };
+  }
+}
