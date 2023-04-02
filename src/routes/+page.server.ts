ea7e40472ff34bfcc3ccbfef97adbb12f36740d3
Markus Obermaier <markus.obermaier420@gmail.com>
2023-04-02 11:19:36 +0200

    file upload

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,23 +1,34 @@
-import type { Actions } from './$types';
+import * as fs from 'fs';
 
 export const actions: Actions = {
-	upload: async ({ cookies, request }) => {
-		const data = await request.formData();
-		const file = data.get('pdf');
-		const filePath = `docs/${file.name}`;
-		const writeStream = fs.createWriteStream(filePath);
-	
-		await new Promise((resolve, reject) => {
-		  // Pipe the file data to the write stream
-		  file.pipe(writeStream);
-	
-		  // Handle any errors that may occur while writing the file
-		  writeStream.on('error', reject);
-	
-		  // Handle the successful write of the file
-		  writeStream.on('finish', resolve);
-		});
-	
-		return "File uploaded successfully";
-	  }
-} satisfies Actions;
+  upload: async ({ cookies, request }) => {
+    const data = await request.formData();
+    const file = data.get('pdf');
+    if (!file) {
+      throw new Error('no file available');
+    }
+  
+    const fileName = file.name;
+    const fileContent = file instanceof Blob ? file : file.buffer;
+    console.log("file name ", fileName)
+    console.log("file content ", fileContent)
+    const fileObject = new File([fileContent], fileName);
+
+    // Create the directory if it doesn't exist
+    await fs.promises.mkdir('docs', { recursive: true });
+    const filePath = `docs/${file.name}`;
+    const writeStream = fs.createWriteStream(filePath);
+
+    await new Promise<void>((resolve, reject) => {
+      readStream.pipe(writeStream);
+
+      readStream.on('error', reject);
+
+      writeStream.on('error', reject);
+
+      writeStream.on('finish', resolve);
+    });
+
+    return 'File uploaded successfully';
+  },
+};
