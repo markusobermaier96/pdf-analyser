e1f47a78135597c0ec6438d8b3e82983269e97f3
Markus Obermaier <markus.obermaier420@gmail.com>
2023-04-16 13:01:14 +0200

    user authentication

--- src/routes/api/auth/+server.ts
+++ src/routes/api/auth/+server.ts
@@ -1,5 +1,5 @@
 import type { RequestHandler } from '@sveltejs/kit';
-
+// TODO: finish user authentication
 export const POST: RequestHandler = async ({ request }) => {
   const { signed } = await request.json();
   console.log(signed);
