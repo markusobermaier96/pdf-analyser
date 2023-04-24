cbd04169035503678e9c138dfa2a5d05d2654d6c
Markus Obermaier <markus.obermaier420@gmail.com>
2023-04-24 10:12:19 +0200

    removed unused code in store

--- src/lib/store/messageStore.ts
+++ src/lib/store/messageStore.ts
@@ -1,41 +1,6 @@
 import { writable } from 'svelte/store';
 import type { ChatCompletionRequestMessage, ChatCompletionResponseMessageRoleEnum } from 'openai';
 
-/* interface Message extends ChatCompletionRequestMessage {
-	err?: boolean;
-}
-
-interface MessageStore {
-	messages: Message[];
-} */
-
-
-/* 
-export const messageStore = writable<MessageStore>({
-	messages: [
-		{
-			content: 'Hi, I am your pdf analysis assistant. How can I help you?',
-			role:  'assistant',
-		}
-	],
-}); */
-
-/* export const appendMessage = (content: string, role: ChatCompletionResponseMessageRoleEnum, err?: boolean) => {
-	messageStore.update((store) => {
-		return {
-			...store,
-			messages: [
-				...store.messages,
-				{
-					content,
-					role,
-					err
-				}
-			]
-		};
-	});
-}; */
-
 export const messageStore = writable<ChatCompletionRequestMessage[]>([{
 	content: 'Hi, I am your pdf analysis assistant. How can I help you?',
 	role:  'assistant',
