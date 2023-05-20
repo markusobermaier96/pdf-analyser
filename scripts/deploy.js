abf7e11f6000e484b760370a7bd5283be3918c21
Markus Obermaier <markus.obermaier420@gmail.com>
2023-05-20 21:42:48 +0200

    added compile task automation

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,10 +1,7 @@
-const hre = require('hardhat');
+//const hre = require('hardhat');
 
 async function main() {
-	const Token = await hre.ethers.getContractFactory('Token');
-	const token = await Token.deploy();
-	await token.deployed();
-	console.log('Token deployed to:', token.address);
+    
 }
 
 main()
